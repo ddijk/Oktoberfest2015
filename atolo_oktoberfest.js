b0fa691a9d7bac1c9fa0e92f402289206b8d57fe
ddijk <dick@dijkrosoft.nl>
2015-06-02 20:51:01 +0200

    versie 2014

---
+++ atolo_oktoberfest.js
@@ -0,0 +1,83 @@
+
+Wordpress, vulling Theme Options->Section About:
+<br>
+<p class="of-1">
+Oktoberfest de Nachtzoen is een initiatief van de <a href='https://www.lions.nl/utrechtnovum' target="_blank">Mixed Lions Club Utrecht Novum</a>
+</p>
+<br>
+<p class="of-1">
+Het wordt een supertolles fest met bier, schnapps, muziek (DJ Mikey, de Bloaskapel en oktoberfestband Woenderbaar), bratwurst, dirndls und weiter..
+</p>
+<p class="of-1">
+Kortom alle ingrediënten voor een heel leuk feest met je vrienden, buren en bekenden! 
+</p>
+<p class="of-1">
+<b>
+Zaterdag 11 oktober 2014 
+Cultureel centrum Azotod, Meerndijk 126, 3454 HV De Meern 19.30 – 01.00 uur 
+</b>
+</p>
+<p class="of-1">
+De minimum leeftijd voor het Oktoberfest is 18 jaar. Bij de ingang kan door de beveiliging gevraagd worden naar een geldige legitimatie. 
+</p>
+
+<p class="of-1">
+Klik <a href="#huisregels" onclick="$('#of-huisregels').toggle();">hier</a> voor de huisregels
+</p>
+<p align="center"><img src="http://oktoberfestdenachtzoen.nl/wp-content/uploads/2014/09/logo-Azotod.jpg"></p>
+<script type="text/javascript">$(function() { 
+$('#about').find('h3.section-quote').remove();
+$('#services').find('h3.section-quote').remove();
+
+$('#clients').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+$('#clients').find('.row').children().remove();
+var of_client=$('#clients').find('.row');
+$("<div class='span12'><h2 class='section-title'>Bereikbaarheid</h2><div class='row'><p class='of-1'>De Azotod ligt naast de A12 en is uitstekend bereikbaar per fiets, openbaar vervoer en auto. Parkeren is gratis.</p><p class='of-1'><b>Routebeschrijving voor auto\'s</b></p><p class='of-1'>Vanuit Amsterdam, Den Bosch en Arnhem: Ga de A12 op richting Den Haag en neem afslag 15 De Meern. Onderaan de afslag rechtsaf en bij de volgende stoplichten linksaf. Je ziet Azotod nu aan je rechterhand liggen.</p><p class='of-1'>Vanuit Den Haag: Ga de A12 op richting Utrecht en neem afslag 15 De Meern. Onderaan de afslag linksaf en bij de volgende stoplichten rechtdoor. Je ziet Azotod nu aan je rechterhand liggen.</p><p class='of-1'><b>Openbaar Vervoer</b></p><p class='of-1'>Vanuit Utrecht bus 107 /407 (Connexxion), deze stopt voor de deur (halte De Meern Zuid).<br>Bushalte De Meern Zuid (dus voor de deur) lijn 107 gaat m 00.07 naar Utrecht<br>Bushalte Meernbrug (15 minuten lopen) lijn 28 gaat om 00.04,00.34, 00.50 en lijn 102 gaat nog om 00.00, 01.00, 02.00 richting Utrecht<br>Bus 407 stopt voor onze deur, andere nachtbussen stoppen in de omgeving van Azotod (407, 402) <br></p><p class='of-1'>Ook andere bussen richting De Meern zijn mogelijk, maar dan moet je vanaf de kruising Rijksstraatweg/Meerndijk ongeveer 8 minuten lopen.</p><p class='of-1'><b>Nightliners</b><br>Op zaterdag rijden er speciale nachtbussen van en naar de omgeving van Azotod in de richting van  Utrecht, Montfoort, Vleuten, Woerden en Maarssen.</p><p class='of-1'><b>Hotel</b><br>De dichtstbijzijnde gelegenheid om te overnachten is Hotel Holiday Inn op kantorenpark Papendorp, Utrecht.</p></div></div>").appendTo(of_client);
+
+$('#blog').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+$('#blog').find('.row').children().remove();
+var of_blog=$('#blog').find('.row');
+$("<div class='span12'><h2 class='section-title'>Kaarten</h2><div class='row'><p class='of-1'>Kaarten in de voorverkoop &euro;15<br>Kaarten aan de deur &euro;20</p><p class='of-1'>Fysieke verkooppunten:<br>Kantoorboekhandel Kees Visscher vestiging Vleuterweide en vestiging De Meern, restaurant De Brouwerij Vleuten, Filmclub Parkwijk, Primera Veldhuizen en Jones en Jones Vleuterweide en Nieuwegein.</p><p class='of-1'>Online verkooppunten:<br><a href='http://www.azotod.nl/tickets' target='_blank'>Azotod</a></p><p class='of-1'><b>Betalen</b><br>Je kunt munten kopen bij de muntenkassa\'s en er kan met pin of contant worden afgerekend</p></div></div>").appendTo(of_blog);
+
+$('#portfolio').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+$('#portfolio').find('.row').children().remove();
+var of_muziek=$('#portfolio').find('.row');
+$("<div class='span12'><h2 class='section-title'>Muziek</h2><div class='row'><p class='of-1'><a href='http://www.woenderbaar.nl/' target='_blank'>Band Woenderbaar</a>, luister en geniet alvast!<br><a target='_blank' href='https://www.youtube.com/watch?v=N-UWxGIt4R0'>Youtube</a></p><p class='of-1'><a href='http://harmonieorkestvleuten.nl/bloaskapel-vleuten' target='_blank'>Bloaskapel Vleuten</a></p><p class='of-1'>DJ Mikey</p><p align='center'><img src='http://oktoberfestdenachtzoen.nl/wp-content/uploads/2014/09/Woenderbaar-2.jpg'></p></div></div>").appendTo(of_muziek);
+
+$('#pricing').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+$('#pricing').find('.row').children().remove();
+var of_pricing=$('#pricing').find('.row');
+$("<div class='span12'><h2 class='section-title'>Ratjetoe</h2><div class='row'><p class='of-1' align='center'><img src='http://oktoberfestdenachtzoen.nl/wp-content/uploads/2014/09/ratjetoe.jpg'></p></div></div>").appendTo(of_pricing);
+
+$('#social-media').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+       $('#social-media').find('.row').children().remove();
+       var of_sponsors=$('#social-media').find('.row');
+       $("<div class='span12'><h2 class='section-title'>Sponsors</h2><div class='row'><p class='of-1' style='text-align: center'><i>Hartelijk dank aan alle sponsors</i></p><ul class='of-1' style='text-align: center'><li>Brouwerij Maximus</li><li>AH Vleuten</li><li>Kwalitaria De Meern</li><li>Ruiter Groendecoratie</li><li>Slijterij Van Schaik</li><li>Banketbakkerij Van Straaten</li><li>Bakkerij Verkerk</li><li>Slagerij Oostveen</li><li>Zeeman</li><li>Kruidvat</li><li>Opticien Kreft</li><li>Europarts</li><li>Zomer IJssalon</li><li>Slagerij Kees Vernooij</li><li>Zoen</li><li>Boulevard</li><li>Leeuwis Tweewielers</li><li>Hansen Verf &amp; Wand</li><li>DA Drogist Vleuten</li><li>Italiaans restaurant D&apos;Amici</li><li>Bos Groenteman</li><li>Jones &amp; Jones</li></ul></div></div>").appendTo(of_sponsors);
+     
+
+});</script>
+<div style="margin-bottom: 100px">
+<a name="huisregels" />
+</div>
+<br>
+<div id="of-huisregels" style="display: none">
+<h2>Huisregels</h2>
+<ol>
+<li>Houd je altijd aan de aanwijzingen van de organisatie en beveiliging.</li>
+<li>Houd rekening met andere bezoekers en ga respectvol met elkaar om.</li>
+<li>De organisatie is niet aansprakelijk voor letsel, beschadiging of het kwijtraken</li>
+van persoonlijke eigendommen als gevolg van bezoek aan het Oktoberfest.</li>
+<li>Het meebrengen van eigen consumpties is niet toegestaan.</li>
+<li>Drugs, wapens, glaswerk en blik zijn verboden.</li>
+<li>Bij betreding van de Azotod kun je gefouilleerd worden door de beveiliging.</li>
+<li>Wie geen medewerking verleent aan de controle van kleding en aan het
+fouilleren kan de toegang worden ontzegd.</li>
+<li>De organisatie kan personen de toegang weigeren.</li>
+<li>Door betreden van de Azotod geef je toestemming om in de Azotod gemaakte
+foto’s te gebruiken voor publicatiedoeleinden.</li>
+<li>Flyeren is verboden.</li>
+</ol>
+</div>
+
+
+
