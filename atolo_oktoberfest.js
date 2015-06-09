64afedee9cf1a3df01eb4a1d5c916a6720848029
ddijk <dick@dijkrosoft.nl>
2015-06-09 21:56:24 +0200

    teksten, sponsor plaatje

--- atolo_oktoberfest.js
+++ atolo_oktoberfest.js
@@ -1,92 +1,92 @@
-<!-- Wordpress, vulling Theme Options->Section About: -->
-<br>
-<p class="of-1">
-Oktoberfest de Nachtzoen is een initiatief van de <a href='https://www.lions.nl/utrechtnovum' target="_blank">Mixed Lions Club Utrecht Novum</a>Alle opbrengsten van dit supertolles fest gaan naar het goede doel ‘De Nachtzoen’ van het Ronald McDonald Huis te Utrecht. Kijk voor meer info op het tabje ‘Het Goede Doel’.
-</p>
-<br>
-<p class="of-1">
-We gaan Party machen met bier, schnapps, muziek (DJ Mikey, de Bloaskapel en oktoberfestband Woenderbaar), bratwurst, dirndls und weiter..
-</p>
-<p class="of-1">
-Kortom alle ingrediënten voor een heel leuk feest met je vrienden, buren en bekenden! 
-</p>
-<p class="of-1">
-<b>
-Zaterdag 26 september 2015 
-Cultureel centrum Azotod, Meerndijk 126, 3454 HV De Meern 20.30 – 01.00 uur 
-</b>
-</p>
-<p class="of-1">
-De minimum leeftijd voor het Oktoberfest is 18 jaar. Bij de ingang kan door de beveiliging gevraagd worden naar een geldige legitimatie. 
-</p>
+	<!-- Wordpress, vulling Theme Options->Section About: -->
+	<br>
+	<p class="of-1">
+	Oktoberfest de Nachtzoen is een initiatief van de <a href='https://www.lions.nl/utrechtnovum' target="_blank">Mixed Lions Club Utrecht Novum</a>Alle opbrengsten van dit supertolles fest gaan naar het goede doel ‘De Nachtzoen’ van het Ronald McDonald Huis te Utrecht. Kijk voor meer info op het tabje ‘Het Goede Doel’.
+	</p>
+	<br>
+	<p class="of-1">
+	We gaan Party machen met bier, schnapps, muziek (DJ Mikey, de Bloaskapel en oktoberfestband Woenderbaar), bratwurst, dirndls und weiter..
+	</p>
+	<p class="of-1">
+	Kortom alle ingrediënten voor een heel leuk feest met je vrienden, buren en bekenden! 
+	</p>
+	<p class="of-1">
+	<b>
+	Zaterdag 26 september 2015 
+	Cultureel centrum Azotod, Meerndijk 126, 3454 HV De Meern 20.30 – 01.00 uur 
+	</b>
+	</p>
+	<p class="of-1">
+	De minimum leeftijd voor het Oktoberfest is 18 jaar. Bij de ingang kan door de beveiliging gevraagd worden naar een geldige legitimatie. 
+	</p>
 
-<p class="of-1">
-Klik <a href="#huisregels" onclick="$('#of-huisregels').toggle();">hier</a> voor de huisregels
-</p>
-<p align="center"><!--<img src="http://oktoberfestdenachtzoen.nl/wp-content/uploads/2014/09/logo-Azotod.jpg">--></p>
-<script type="text/javascript">$(function() { 
-	$('.col-sm-6').hide();
-$('.caption-block').hide();
-$('#about').find('h3.section-quote').remove();
+	<p class="of-1">
+	Klik <a href="#huisregels" onclick="$('#of-huisregels').toggle();">hier</a> voor de huisregels
+	</p>
+	<p align="center"><!--<img src="http://oktoberfestdenachtzoen.nl/wp-content/uploads/2014/09/logo-Azotod.jpg">--></p>
+	<script type="text/javascript">$(function() { 
+		$('.col-sm-6').hide();
+	$('.caption-block').hide();
+	$('#about').find('h3.section-quote').remove();
 
-// Goede doel:
-$('#services').find('h3.section-quote').remove();
+	// Goede doel:
+	$('#services').find('h3.section-quote').remove();
 
-$('#services').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
-$('#services').find('.row').children().remove();
-var of_services=$('#services').find('.row');
-$("<div class='span12'><h2 class='section-title'>Goede doel</h2><div class='row'><p class='of-1'>Een kus, een knuffel en een paar lieve woordjes voor het slapen gaan zijn voor ieder kind belangrijk en zeker voor een ziek kind dat wekenlang in een ziekenhuis ligt en wil dat papa, mama en hun broertje of zusje in de buurt zijn. Voor hen is een nachtzoen van onschatbare waarde. Het Ronald McDonald Huis Utrecht maakt dit mogelijk voor een kind dat in het ernaast gelegen Wilhelmina Kinderziekenhuis verblijft. Zo kan een gezin heel dicht bij elkaar blijven in een voor hen vaak moeilijke periode. Dit kan alleen dankzij de inzet van ruim 130 vrijwilligers en donaties van particulieren, bedrijven en serviceclubs. <br> <br>Onderstaand een greep uit het gastenboek, dan wordt overduidelijk hoeveel dit voor mensen betekent.  <br><b>Sara, 6 jaar</b><br> Hoi, ik ben Sara en ik ben 6 jaar. Ik moest een paar weken in het ziekenhuis blijven en elke dag kwam ik even hier met mijn papa of mama om even spelletjes te doen. Dat was erg leuk. Het is fijn om hier heen te kunnen en niet steeds op je kamer te moeten blijven.  <br>Karin, 37 jaar Groot<br>respect! Bedankt voor alles wat jullie betekenen, bedankt voor alles wat jullie doen. Jullie maken het verschil!  Michael en Anouk, 32 jaar Fijn dat deze huizen bestaan. Onze pasgeboren baby die in het ziekenhuis ligt en wij op een kleine afstand ernaast. Je voelt je dan heel dichtbij je kleine mannetje. We hebben in dit huis erg veel rust gekregen. Dank aan alle vrijwilligers voor het fantastische werk wat jullie doen. We vonden het heel erg fijn hier te mogen zijn. We noemde het zelfs “thuis’’.  Aaron en Elise, 41 jaar Wat ontzettend fijn dat er een plek als deze is! Wat een zegen is het voor ouders om gebruik te mogen maken van dit huis en al haar faciliteiten. Het is zo mooi, dat er mensen zijn die hun tijd hierin steken. Wij zijn erg dankbaar voor de momenten die wij hier mochten zijn.  Laurens en Angelique, 36 jaar In het ziekenhuis zorgen ze voor ons kereltje, maar in dit huis worden wij een beetje verzorgd. Jullie betekenen heel veel voor ons! Bedankt en ga zo door.  Kay en Rosa, 29 jaar Ons zoontje heeft na zijn geboorte een zware en moeilijke start gehad. Het Ronald McDonald huis is daarom voor ons tweede huis geworden. We gaan het missen hier. Er zijn bijzondere vriendschappen ontstaan. Voor ons zijn jullie ontzettend bijzonder!  </p> </div></div> ").appendTo(of_services);
+	$('#services').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+	$('#services').find('.row').children().remove();
+	var of_services=$('#services').find('.row');
+	$("<div class='span12'><h2 class='section-title'>Goede doel</h2><div class='row'><p class='of-1'>Een kus, een knuffel en een paar lieve woordjes voor het slapen gaan zijn voor ieder kind belangrijk en zeker voor een ziek kind dat wekenlang in een ziekenhuis ligt en wil dat papa, mama en hun broertje of zusje in de buurt zijn. Voor hen is een nachtzoen van onschatbare waarde. Het Ronald McDonald Huis Utrecht maakt dit mogelijk voor een kind dat in het ernaast gelegen Wilhelmina Kinderziekenhuis verblijft. Zo kan een gezin heel dicht bij elkaar blijven in een voor hen vaak moeilijke periode. Dit kan alleen dankzij de inzet van ruim 130 vrijwilligers en donaties van particulieren, bedrijven en serviceclubs. <br> <br>Onderstaand een greep uit het gastenboek, dan wordt overduidelijk hoeveel dit voor mensen betekent.  <br><b>Sara, 6 jaar</b><br> Hoi, ik ben Sara en ik ben 6 jaar. Ik moest een paar weken in het ziekenhuis blijven en elke dag kwam ik even hier met mijn papa of mama om even spelletjes te doen. Dat was erg leuk. Het is fijn om hier heen te kunnen en niet steeds op je kamer te moeten blijven.  <br>Karin, 37 jaar Groot<br>respect! Bedankt voor alles wat jullie betekenen, bedankt voor alles wat jullie doen. Jullie maken het verschil!  Michael en Anouk, 32 jaar Fijn dat deze huizen bestaan. Onze pasgeboren baby die in het ziekenhuis ligt en wij op een kleine afstand ernaast. Je voelt je dan heel dichtbij je kleine mannetje. We hebben in dit huis erg veel rust gekregen. Dank aan alle vrijwilligers voor het fantastische werk wat jullie doen. We vonden het heel erg fijn hier te mogen zijn. We noemde het zelfs “thuis’’.  Aaron en Elise, 41 jaar Wat ontzettend fijn dat er een plek als deze is! Wat een zegen is het voor ouders om gebruik te mogen maken van dit huis en al haar faciliteiten. Het is zo mooi, dat er mensen zijn die hun tijd hierin steken. Wij zijn erg dankbaar voor de momenten die wij hier mochten zijn.  Laurens en Angelique, 36 jaar In het ziekenhuis zorgen ze voor ons kereltje, maar in dit huis worden wij een beetje verzorgd. Jullie betekenen heel veel voor ons! Bedankt en ga zo door.  Kay en Rosa, 29 jaar Ons zoontje heeft na zijn geboorte een zware en moeilijke start gehad. Het Ronald McDonald huis is daarom voor ons tweede huis geworden. We gaan het missen hier. Er zijn bijzondere vriendschappen ontstaan. Voor ons zijn jullie ontzettend bijzonder!  </p> </div></div> ").appendTo(of_services);
 
-// Locatie:
-$('#clients').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
-$('#clients').find('.row').children().remove();
-var of_client=$('#clients').find('.row');
-$("<div class='span12'><h2 class='section-title'>Locatie</h2><div class='row'><p class='of-1'>De Azotod ligt naast de A12 en is uitstekend bereikbaar per fiets, openbaar vervoer en auto. Parkeren is gratis.</p><p class='of-1'><b>Routebeschrijving voor auto\'s</b></p><p class='of-1'>Vanuit Amsterdam, Den Bosch en Arnhem: Ga de A12 op richting Den Haag en neem afslag 15 De Meern. Onderaan de afslag rechtsaf en bij de volgende stoplichten linksaf. Je ziet Azotod nu aan je rechterhand liggen.</p><p class='of-1'>Vanuit Den Haag: Ga de A12 op richting Utrecht en neem afslag 15 De Meern. Onderaan de afslag linksaf en bij de volgende stoplichten rechtdoor. Je ziet Azotod nu aan je rechterhand liggen.</p><p class='of-1'><b>Openbaar Vervoer</b></p><p class='of-1'>Vanuit Utrecht bus 107 /407 (Connexxion), deze stopt voor de deur (halte De Meern Zuid).<br>Bushalte De Meern Zuid (dus voor de deur) lijn 107 gaat m 00.07 naar Utrecht<br>Bushalte Meernbrug (15 minuten lopen) lijn 28 gaat om 00.04,00.34, 00.50 en lijn 102 gaat nog om 00.00, 01.00, 02.00 richting Utrecht<br>Bus 407 stopt voor onze deur, andere nachtbussen stoppen in de omgeving van Azotod (407, 402) <br></p><p class='of-1'>Ook andere bussen richting De Meern zijn mogelijk, maar dan moet je vanaf de kruising Rijksstraatweg/Meerndijk ongeveer 8 minuten lopen.</p><p class='of-1'><b>Nightliners</b><br>Op zaterdag rijden er speciale nachtbussen van en naar de omgeving van Azotod in de richting van  Utrecht, Montfoort, Vleuten, Woerden en Maarssen.</p><p class='of-1'><b>Hotel</b><br>De dichtstbijzijnde gelegenheid om te overnachten is Hotel Holiday Inn op kantorenpark Papendorp, Utrecht.</p></div></div>").appendTo(of_client);
+	// Locatie:
+	$('#clients').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+	$('#clients').find('.row').children().remove();
+	var of_client=$('#clients').find('.row');
+	$("<div class='span12'><h2 class='section-title'>Locatie</h2><div class='row'><p class='of-1'>De Azotod ligt naast de A12 en is uitstekend bereikbaar per fiets, openbaar vervoer en auto. Parkeren is gratis.</p><p class='of-1'><b>Routebeschrijving voor auto\'s</b></p><p class='of-1'>Vanuit Amsterdam, Den Bosch en Arnhem: Ga de A12 op richting Den Haag en neem afslag 15 De Meern. Onderaan de afslag rechtsaf en bij de volgende stoplichten linksaf. Je ziet Azotod nu aan je rechterhand liggen.</p><p class='of-1'>Vanuit Den Haag: Ga de A12 op richting Utrecht en neem afslag 15 De Meern. Onderaan de afslag linksaf en bij de volgende stoplichten rechtdoor. Je ziet Azotod nu aan je rechterhand liggen.</p><p class='of-1'><b>Openbaar Vervoer</b></p><p class='of-1'>Vanuit Utrecht bus 107 /407 (Connexxion), deze stopt voor de deur (halte De Meern Zuid).<br>Bushalte De Meern Zuid (dus voor de deur) lijn 107 gaat m 00.07 naar Utrecht<br>Bushalte Meernbrug (15 minuten lopen) lijn 28 gaat om 00.04,00.34, 00.50 en lijn 102 gaat nog om 00.00, 01.00, 02.00 richting Utrecht<br>Bus 407 stopt voor onze deur, andere nachtbussen stoppen in de omgeving van Azotod (407, 402) <br></p><p class='of-1'>Ook andere bussen richting De Meern zijn mogelijk, maar dan moet je vanaf de kruising Rijksstraatweg/Meerndijk ongeveer 8 minuten lopen.</p><p class='of-1'><b>Nightliners</b><br>Op zaterdag rijden er speciale nachtbussen van en naar de omgeving van Azotod in de richting van  Utrecht, Montfoort, Vleuten, Woerden en Maarssen.</p><p class='of-1'><b>Hotel</b><br>De dichtstbijzijnde gelegenheid om te overnachten is Hotel Holiday Inn op kantorenpark Papendorp, Utrecht.</p></div></div>").appendTo(of_client);
 
-// Kaarten:
-$('#blog').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
-$('#blog').find('.row').children().remove();
-var of_blog=$('#blog').find('.row');
-$("<div class='span12'><h2 class='section-title'>Kaarten</h2><div class='row'><p class='of-1'>Kaarten in de voorverkoop &euro;15<br>Kaarten aan de deur &euro;20</p><p class='of-1'><b>Fysieke verkooppunten: </b>Kantoorboekhandel Kees Visscher vestiging Vleuterweide en vestiging De Meern, restaurant De Brouwerij Vleuten en Primera Veldhuizen</p><p class='of-1'><b>Online verkooppunten: </b><a href='http://www.azotod.nl/tickets' target='_blank'>Azotod</a></p><p class='of-1'><b>Betalen: </b>Je kunt munten kopen bij de muntenkassa\'s en er kan met pin of contant worden afgerekend</p></div></div>").appendTo(of_blog);
+	// Kaarten:
+	$('#blog').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+	$('#blog').find('.row').children().remove();
+	var of_blog=$('#blog').find('.row');
+	$("<div class='span12'><h2 class='section-title'>Kaarten</h2><div class='row'><p class='of-1'>Kaarten in de voorverkoop &euro;15<br>Kaarten aan de deur &euro;20</p><p class='of-1'><table id='verkooppunten'><tr><td height='50'><b>Fysieke verkooppunten: </b></td><td height='50'>Kantoorboekhandel Kees Visscher vestiging Vleuterweide en vestiging De Meern, restaurant De Brouwerij Vleuten, Jones&amp;Jones en Primera Veldhuizen</td></tr><tr><td height='50'><b>Online verkooppunten: </b></td><td height='50'><a href='http://www.azotod.nl/tickets' target='_blank'>Azotod</a></td></tr><tr><td height='50'><b>Betalen: </b></td><td height='50'>Je kunt munten kopen bij de muntenkassa\'s en er kan met pin of contant worden afgerekend</td></tr></table></p></div></div>").appendTo(of_blog);
 
-$('#portfolio').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
-$('#portfolio').find('.row').children().remove();
-var of_muziek=$('#portfolio').find('.row');
-$("<div class='span12'><h2 class='section-title'>Muziek</h2><div class='row'><p class='of-1' align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/woenderbaar.png'></p><p class='of-1' align='center'><a href='http://www.woenderbaar.nl/' target='_blank'>Band Woenderbaar</a>, luister en geniet alvast! <a target='_blank' href='https://www.youtube.com/watch?v=N-UWxGIt4R0'>Youtube</a></p><p align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/blaaskapel.jpg'></p><p class='of-1' align='center'><a href='http://harmonieorkestvleuten.nl/bloaskapel-vleuten' target='_blank'>Bloaskapel Vleuten</a></p><p class='of-1' align='center'>DJ Mikey</p></div></div>").appendTo(of_muziek);
+	$('#portfolio').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+	$('#portfolio').find('.row').children().remove();
+	var of_muziek=$('#portfolio').find('.row');
+	$("<div class='span12'><h2 class='section-title'>Muziek</h2><div class='row'><p class='of-1' align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/woenderbaar.png'></p><p class='of-1' align='center'><a href='http://www.woenderbaar.nl/' target='_blank'>Band Woenderbaar</a>, luister en geniet alvast! <a target='_blank' href='https://www.youtube.com/watch?v=N-UWxGIt4R0'>Youtube</a></p><p align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/blaaskapel.jpg'></p><p class='of-1' align='center'><a href='http://harmonieorkestvleuten.nl/bloaskapel-vleuten' target='_blank'>Bloaskapel Vleuten</a></p><p class='of-1' align='center'>DJ Mikey</p></div></div>").appendTo(of_muziek);
 
-$('#pricing').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
-$('#pricing').find('.row').children().remove();
-
-
-$('#social-media').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
-       $('#social-media').find('.row').children().remove();
-       var of_sponsors=$('#social-media').find('.row');
-       $("<div class='span12'><h2 class='section-title'>Sponsors</h2><div class='row'><p class='of-1' align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/BNid-OFDN-Sponsoren-hoofd2.jpg'></div></div>").appendTo(of_sponsors);
-     
-
-});
+	$('#pricing').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+	$('#pricing').find('.row').children().remove();
 
 
-</script>
-<div style="margin-bottom: 100px">
-<a name="huisregels" />
-</div>
-<br>
-<div id="of-huisregels" style="display: none">
-<h2>Huisregels</h2>
-<ol>
-<li>Houd je altijd aan de aanwijzingen van de organisatie en beveiliging.</li>
-<li>Houd rekening met andere bezoekers en ga respectvol met elkaar om.</li>
-<li>De organisatie is niet aansprakelijk voor letsel, beschadiging of het kwijtraken</li>
-van persoonlijke eigendommen als gevolg van bezoek aan het Oktoberfest.</li>
-<li>Het meebrengen van eigen consumpties is niet toegestaan.</li>
-<li>Drugs, wapens, glaswerk en blik zijn verboden.</li>
-<li>Bij betreding van de Azotod kun je gefouilleerd worden door de beveiliging.</li>
-<li>Wie geen medewerking verleent aan de controle van kleding en aan het
-fouilleren kan de toegang worden ontzegd.</li>
-<li>De organisatie kan personen de toegang weigeren.</li>
-<li>Door betreden van de Azotod geef je toestemming om in de Azotod gemaakte
-foto’s te gebruiken voor publicatiedoeleinden.</li>
-<li>Flyeren is verboden.</li>
-</ol>
-</div>
+	$('#social-media').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
+	       $('#social-media').find('.row').children().remove();
+	       var of_sponsors=$('#social-media').find('.row');
+	       $("<div class='span12'><h2 class='section-title'>Sponsors</h2><div class='row'><p class='of-1' align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/BNid-OFDN-Sponsoren-hoofd2.jpg'></div></div>").appendTo(of_sponsors);
+	     
+
+	});
+
+
+	</script>
+	<div style="margin-bottom: 100px">
+	<a name="huisregels" />
+	</div>
+	<br>
+	<div id="of-huisregels" style="display: none">
+	<h2>Huisregels</h2>
+	<ol>
+	<li>Houd je altijd aan de aanwijzingen van de organisatie en beveiliging.</li>
+	<li>Houd rekening met andere bezoekers en ga respectvol met elkaar om.</li>
+	<li>De organisatie is niet aansprakelijk voor letsel, beschadiging of het kwijtraken</li>
+	van persoonlijke eigendommen als gevolg van bezoek aan het Oktoberfest.</li>
+	<li>Het meebrengen van eigen consumpties is niet toegestaan.</li>
+	<li>Drugs, wapens, glaswerk en blik zijn verboden.</li>
+	<li>Bij betreding van de Azotod kun je gefouilleerd worden door de beveiliging.</li>
+	<li>Wie geen medewerking verleent aan de controle van kleding en aan het
+	fouilleren kan de toegang worden ontzegd.</li>
+	<li>De organisatie kan personen de toegang weigeren.</li>
+	<li>Door betreden van de Azotod geef je toestemming om in de Azotod gemaakte
+	foto’s te gebruiken voor publicatiedoeleinden.</li>
+	<li>Flyeren is verboden.</li>
+	</ol>
+	</div>
