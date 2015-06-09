9cb29a1a519475890094cd281428c32bb5b10a3c
ddijk <dick@dijkrosoft.nl>
2015-06-09 21:21:20 +0200

    teksten, sponsor plaatje

--- atolo_oktoberfest.js
+++ atolo_oktoberfest.js
@@ -1,11 +1,11 @@
 <!-- Wordpress, vulling Theme Options->Section About: -->
 <br>
 <p class="of-1">
-Oktoberfest de Nachtzoen is een initiatief van de <a href='https://www.lions.nl/utrechtnovum' target="_blank">Mixed Lions Club Utrecht Novum</a>
+Oktoberfest de Nachtzoen is een initiatief van de <a href='https://www.lions.nl/utrechtnovum' target="_blank">Mixed Lions Club Utrecht Novum</a>Alle opbrengsten van dit supertolles fest gaan naar het goede doel ‘De Nachtzoen’ van het Ronald McDonald Huis te Utrecht. Kijk voor meer info op het tabje ‘Het Goede Doel’.
 </p>
 <br>
 <p class="of-1">
-Het wordt een supertolles fest met bier, schnapps, muziek (DJ Mikey, de Bloaskapel en oktoberfestband Woenderbaar), bratwurst, dirndls und weiter..
+We gaan Party machen met bier, schnapps, muziek (DJ Mikey, de Bloaskapel en oktoberfestband Woenderbaar), bratwurst, dirndls und weiter..
 </p>
 <p class="of-1">
 Kortom alle ingrediënten voor een heel leuk feest met je vrienden, buren en bekenden! 
@@ -35,7 +35,7 @@ $('#services').find('h3.section-quote').remove();
 $('#services').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
 $('#services').find('.row').children().remove();
 var of_services=$('#services').find('.row');
-$("<div class='span12'><h2 class='section-title'>Goede doel</h2><div class='row'><p class='of-1'>Als je kind ziek is, wil je als ouder zo dicht mogelijk in de buurt zijn.</p><p class='of-1'>Je wilt binnen een paar minuten aan de rand van het bed staan als je kind je nodig heeft. </p><p class='of-1'Daarom zijn de Ronald McDonald Huizen onmisbaar.</p></div></div>").appendTo(of_services);
+$("<div class='span12'><h2 class='section-title'>Goede doel</h2><div class='row'><p class='of-1'>Een kus, een knuffel en een paar lieve woordjes voor het slapen gaan zijn voor ieder kind belangrijk en zeker voor een ziek kind dat wekenlang in een ziekenhuis ligt en wil dat papa, mama en hun broertje of zusje in de buurt zijn. Voor hen is een nachtzoen van onschatbare waarde. Het Ronald McDonald Huis Utrecht maakt dit mogelijk voor een kind dat in het ernaast gelegen Wilhelmina Kinderziekenhuis verblijft. Zo kan een gezin heel dicht bij elkaar blijven in een voor hen vaak moeilijke periode. Dit kan alleen dankzij de inzet van ruim 130 vrijwilligers en donaties van particulieren, bedrijven en serviceclubs. <br> <br>Onderstaand een greep uit het gastenboek, dan wordt overduidelijk hoeveel dit voor mensen betekent.  <br><b>Sara, 6 jaar</b><br> Hoi, ik ben Sara en ik ben 6 jaar. Ik moest een paar weken in het ziekenhuis blijven en elke dag kwam ik even hier met mijn papa of mama om even spelletjes te doen. Dat was erg leuk. Het is fijn om hier heen te kunnen en niet steeds op je kamer te moeten blijven.  <br>Karin, 37 jaar Groot<br>respect! Bedankt voor alles wat jullie betekenen, bedankt voor alles wat jullie doen. Jullie maken het verschil!  Michael en Anouk, 32 jaar Fijn dat deze huizen bestaan. Onze pasgeboren baby die in het ziekenhuis ligt en wij op een kleine afstand ernaast. Je voelt je dan heel dichtbij je kleine mannetje. We hebben in dit huis erg veel rust gekregen. Dank aan alle vrijwilligers voor het fantastische werk wat jullie doen. We vonden het heel erg fijn hier te mogen zijn. We noemde het zelfs “thuis’’.  Aaron en Elise, 41 jaar Wat ontzettend fijn dat er een plek als deze is! Wat een zegen is het voor ouders om gebruik te mogen maken van dit huis en al haar faciliteiten. Het is zo mooi, dat er mensen zijn die hun tijd hierin steken. Wij zijn erg dankbaar voor de momenten die wij hier mochten zijn.  Laurens en Angelique, 36 jaar In het ziekenhuis zorgen ze voor ons kereltje, maar in dit huis worden wij een beetje verzorgd. Jullie betekenen heel veel voor ons! Bedankt en ga zo door.  Kay en Rosa, 29 jaar Ons zoontje heeft na zijn geboorte een zware en moeilijke start gehad. Het Ronald McDonald huis is daarom voor ons tweede huis geworden. We gaan het missen hier. Er zijn bijzondere vriendschappen ontstaan. Voor ons zijn jullie ontzettend bijzonder!  </p> </div></div> ").appendTo(of_services);
 
 // Locatie:
 $('#clients').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
@@ -52,7 +52,7 @@ $("<div class='span12'><h2 class='section-title'>Kaarten</h2><div class='row'><p
 $('#portfolio').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
 $('#portfolio').find('.row').children().remove();
 var of_muziek=$('#portfolio').find('.row');
-$("<div class='span12'><h2 class='section-title'>Muziek</h2><div class='row'><p class='of-1'><a href='http://www.woenderbaar.nl/' target='_blank'>Band Woenderbaar</a>, luister en geniet alvast!<br><a target='_blank' href='https://www.youtube.com/watch?v=N-UWxGIt4R0'>Youtube</a></p><p class='of-1'><a href='http://harmonieorkestvleuten.nl/bloaskapel-vleuten' target='_blank'>Bloaskapel Vleuten</a></p><p class='of-1'>DJ Mikey</p><p align='center'><!--<img src='http://oktoberfestdenachtzoen.nl/wp-content/uploads/2014/09/Woenderbaar-2.jpg'>--></p></div></div>").appendTo(of_muziek);
+$("<div class='span12'><h2 class='section-title'>Muziek</h2><div class='row'><p class='of-1' align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/woenderbaar.png'></p><p class='of-1' align='center'><a href='http://www.woenderbaar.nl/' target='_blank'>Band Woenderbaar</a>, luister en geniet alvast! <a target='_blank' href='https://www.youtube.com/watch?v=N-UWxGIt4R0'>Youtube</a></p><p align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/blaaskapel.jpg'></p><p class='of-1' align='center'><a href='http://harmonieorkestvleuten.nl/bloaskapel-vleuten' target='_blank'>Bloaskapel Vleuten</a></p><p class='of-1' align='center'>DJ Mikey</p></div></div>").appendTo(of_muziek);
 
 $('#pricing').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
 $('#pricing').find('.row').children().remove();
@@ -61,7 +61,7 @@ $('#pricing').find('.row').children().remove();
 $('#social-media').css('background-image', 'none').css('background-color', '#fff').removeClass('shadow');
        $('#social-media').find('.row').children().remove();
        var of_sponsors=$('#social-media').find('.row');
-       $("<div class='span12'><h2 class='section-title'>Sponsors</h2><div class='row'><p class='of-1' style='text-align: center'><i>Hartelijk dank aan alle sponsors</i></p><ul class='of-1' style='text-align: center'><li>Brouwerij Maximus</li><li>AH Vleuten</li><li>Kwalitaria De Meern</li><li>Ruiter Groendecoratie</li><li>Slijterij Van Schaik</li><li>Banketbakkerij Van Straaten</li><li>Bakkerij Verkerk</li><li>Slagerij Oostveen</li><li>Zeeman</li><li>Kruidvat</li><li>Opticien Kreft</li><li>Europarts</li><li>Zomer IJssalon</li><li>Slagerij Kees Vernooij</li><li>Zoen</li><li>Boulevard</li><li>Leeuwis Tweewielers</li><li>Hansen Verf &amp; Wand</li><li>DA Drogist Vleuten</li><li>Italiaans restaurant D&apos;Amici</li><li>Bos Groenteman</li><li>Jones &amp; Jones</li></ul></div></div>").appendTo(of_sponsors);
+       $("<div class='span12'><h2 class='section-title'>Sponsors</h2><div class='row'><p class='of-1' align='center'><img src='http://denachtzoen.nl/wp-content/uploads/2015/05/BNid-OFDN-Sponsoren-hoofd2.jpg'></div></div>").appendTo(of_sponsors);
      
 
 });
